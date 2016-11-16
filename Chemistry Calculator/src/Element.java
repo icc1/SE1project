bd90864379dc094ca9aee3c80d1f1af8c2b37ab5
Ian Connolly <icc1@students.uwf.edu>
2016-11-16 13:09:05 -0600

    Added Element class and started work on GuiBuilder class

---
+++ Chemistry Calculator/src/Element.java
@@ -0,0 +1,22 @@
+public class Element {
+   
+   private String nameFull;
+   private String nameShort;
+   private int atomicNumber;
+   private double atomicWeight;
+   
+   
+
+	public String getFullName()
+   {
+		return nameFull;
+	}
+   public String getShortName()
+   {
+      return nameShort;
+   }
+   public int getAtomicNumber()
+   {
+      return nameShort;
+   }
+}
--- Chemistry Calculator/src/GuiBuilder.java
+++ Chemistry Calculator/src/GuiBuilder.java
@@ -1,4 +1,29 @@
 
 public class GuiBuilder {
 
+   public void calculatorBuilder()
+   {
+      calcFrame = new JFrame("Java Swing Examples"); 
+      //mainFrame.setSize(400,400); 
+      calcFrame.setLayout(new GridBagLayout(8, 5));
+      calcFrame.setVisible(true);
+      
+      JMenuBar chemBar = new JMenuBar();
+      JMenu chemMenu = new JMenu("Chemistry");
+      
+      JMenuItem periodicItem = new JMenuItem("Periodic Table");
+      JMenuItem molarMassItem = new JMenuItem("Molar Mass Calculator");
+      JMenuItem molecularWeightItem = new JMenuItem("Molecular Weight Calculator");
+      
+      chemMenu.add(periodicItem);
+      chemMenu.add(molarMassItem);
+      chemMenu.add(molecularWeightItem);
+      
+      
+
+
+   }
+   
+   
+
 }
