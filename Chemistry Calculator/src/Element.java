bf5904e5db329022a668d5b0256d1672461b64c7
cjh41 <cjh41@students.uwf.edu>
2016-11-21 13:24:31 -0600

    Added getters and setters
    Added getters and setters

--- Chemistry Calculator/src/Element.java
+++ Chemistry Calculator/src/Element.java
@@ -1,21 +1,42 @@
-public class Element {
-	private String nameFull;
-	private String nameShort;
-	private int atomicNumber;
-	private double atomicWeight;
+public class Element 
+{
+    private String nameFull;
+    private String nameShort;
+    private int atomicNumber;
+    private double atomicWeight;
 	   
 	   
 
-	public String getFullName()
-	{
-		return nameFull;
-	}
-	public String getShortName()
-	{
-	   return nameShort;
-	}
-	public int getAtomicNumber()
-	{
-	   return atomicNumber;
-	}
+    public String getFullName()
+    {
+        return nameFull;
+    }
+    public String getShortName()
+    {
+        return nameShort;
+    }
+    public int getAtomicNumber()
+    {
+        return atomicNumber;
+    }
+    public double getAtomicWeight()
+    {
+        return atomicWeight;
+    }
+    public void setFullName(String name)
+    {
+        nameFull = name;
+    }
+    public void setShortName(String name)
+    {
+        nameShort = name;
+    }
+    public void setAtomicNumber(int number)
+    {
+        atomicNumber = number;
+    }
+    public void setAtmoicWeight(double number)
+    {
+        atomicWeight = number;
+    }
 }
