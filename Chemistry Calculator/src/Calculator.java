3e95f4ee2ae4cbd1d6e0c5cdfb3b1de2c836b675
cjh41 <cjh41@students.uwf.edu>
2016-11-29 00:16:57 -0600

    added methods

--- Chemistry Calculator/src/Calculator.java
+++ Chemistry Calculator/src/Calculator.java
@@ -2,5 +2,33 @@ import java.lang.Math;
 
 public class Calculator 
 {
-
+    public static double cos(double a)
+    {
+        return a;
+    }
+    
+    public static double tan(double a)
+    {
+        return a;
+    }
+    
+    public static double sin(double a)
+    {
+        return a;
+    }
+    
+    public static double log(double a)
+    {
+        return a;
+    }
+    
+    public static double log10(double a)
+    {
+        return a;
+    }
+    
+    public double exp(double base, double power)
+    {
+        return a;
+    }
 }
