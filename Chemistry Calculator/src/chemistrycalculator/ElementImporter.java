2eef61cbb430c9f9ec1cb98ef3115377033147ea
icc1 <icc1@students.uwf.edu>
2016-12-02 22:34:59 -0600

    Updated code to work with included text file
    Also included downloadable jar

--- Chemistry Calculator/src/chemistrycalculator/ElementImporter.java
+++ Chemistry Calculator/src/chemistrycalculator/ElementImporter.java
@@ -1,32 +1,27 @@
 package chemistrycalculator;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Scanner;
+
+import javax.annotation.Resource;
 
 public class ElementImporter {
 	public void elementArrayBuilder()
 	{
-		try 
+		InputStream is = this.getClass().getClassLoader().getResourceAsStream("ElementsAll.txt");//reads file, assigns values to element array
+		Scanner fread = new Scanner(is);
+		for(int i = 0; i < 118; i++)
 		{
-			FileInputStream fin = new FileInputStream("elementsAll.txt");//reads file, assigns values to element array
-			Scanner fread = new Scanner(fin);
-			for(int i = 0; i < 118; i++)
-			{
-				PeriodicTable.getElement(i).setShortName(fread.next());
-				PeriodicTable.getElement(i).setFullName(fread.next());
-				PeriodicTable.getElement(i).setAtomicNumber(Integer.parseInt(fread.next()));
-				PeriodicTable.getElement(i).setAtmoicWeight(Double.parseDouble(fread.next()));
-				PeriodicTable.getElement(i).setElementGroup(Integer.parseInt(fread.next()));
-				PeriodicTable.getElement(i).setElementPeriod(Double.parseDouble(fread.next()));
-				PeriodicTable.getElement(i).setElementFamily(fread.next().replace('-', ' '));
-				PeriodicTable.getElement(i).setElementEtymology(fread.nextLine());
-			}
-			fread.close();
-		} 
-		catch (FileNotFoundException e) 
-		{
-			e.printStackTrace();
+			PeriodicTable.getElement(i).setShortName(fread.next());
+			PeriodicTable.getElement(i).setFullName(fread.next());
+			PeriodicTable.getElement(i).setAtomicNumber(Integer.parseInt(fread.next()));
+			PeriodicTable.getElement(i).setAtmoicWeight(Double.parseDouble(fread.next()));
+			PeriodicTable.getElement(i).setElementGroup(Integer.parseInt(fread.next()));
+			PeriodicTable.getElement(i).setElementPeriod(Double.parseDouble(fread.next()));
+			PeriodicTable.getElement(i).setElementFamily(fread.next().replace('-', ' '));
+			PeriodicTable.getElement(i).setElementEtymology(fread.nextLine());
 		}
+		fread.close();
 	}
 }
